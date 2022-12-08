ddc8e9bbe4f78a2754612860b5040e1a0194761d
Nacho <isanccal@myuax.com>
2022-12-08 23:20:40 +0100

    Cambio 4

--- laberinto.py
+++ laberinto.py
@@ -15,6 +15,15 @@ def recorre_laberinto(laberinto):
             fila += 1
             movimientos.append('Abajo')
         elif movimientos[-1] != 'Izquierda' and columna + 1 < n and laberinto[fila][columna + 1] != 'X':
-            
+            columna += 1
+            movimientos.append('Derecha')
+        elif movimientos[-1] != 'Abajo' and fila - 1 >= 0 and laberinto[fila - 1][columna] != 'X':
+            fila -= 1
+            movimientos.append('Arriba')
+        elif movimientos[-1] != 'Derecha' and columna - 1 >= 0 and laberinto[fila][columna - 1] != 'X':
+            columna -= 1
+            movimientos.append('Izquierda')
+        
+
     
 
